39ac5ae0995112a912b381788d68353c6c437ee1
oliveur83 <tomolivierpro@gmail.com>
2022-12-15 12:58:22 +0100

    fonction

--- View/pagePrincipale.php
+++ View/pagePrincipale.php
@@ -1,27 +1,31 @@
+
 <!DOCTYPE html>
 <html>
-
-<head>
+    <head>
     <script src="fichier.js" defer></script>
-</head>
-
-<body>
-    <input type="button" value="dé pour avancer " onclick="lance_de_de_avance();" />
-    <input type="button" value="dé pour recharger  " onclick="lance_de_de_recharge();" />
+    </head>
+    <body>
+    <input  type="button" value="dé pour avancer " onclick="lance_de_de_avance();" />
+    <input  type="button" value="dé pour recharger  " onclick="lance_de_de_recharge();" />
     case
     <span id="case"> </span>
+    <br>
     dé
     <span id="de"> </span>
+    <br>
     charge
     <span id="charge"> </span>
+    <br>
     entree tunnel
     <span id="et"> </span>
+    <br>
     sortie tunnel
     <span id="st"> </span>
+    <br>
     case montee
     <span id="cm"> </span>
+    <br>
     case descente
     <span id="cd"> </span>
-</body>
-
+    </body>
 </html>
