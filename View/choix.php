723289704ff8a4088d6d2086e8cdab1f451cc17b
Hugo <hugo.ragiot@gmail.com>
2022-12-16 16:24:23 +0100

    some more css

--- View/choix.php
+++ View/choix.php
@@ -1,26 +1,49 @@
-
 <!DOCTYPE html>
 <html>
-    <head>
-    <script src="../Scripts/vehicule.js" defer></script>
-    <script src="../Scripts/script.js" defer></script>
-    <script src="../Scripts/parametre.js" defer></script>
-    </head>
-    <body>
-    <h1>joueur1<h1>
-    <select id="j1" onchange="choix_j1(this.value)">
-    <option value="voiture">voiture</option>
-    <option value="moto">moto</option>
-    <option value="bus">bus </option>
-    </select>
-    <h1>joueur2<h1>
-    <select id="j2" onchange="choix_j2(this.value)">
-    <option value="voiture">voiture</option>
-    <option value="moto">moto</option>
-    <option value="bus">bus </option>
-    </select>
+
+<head>
+  <script src="../Scripts/vehicule.js" defer></script>
+  <script src="../Scripts/script.js" defer></script>
+  <script src="../Scripts/parametre.js" defer></script>
+  <link rel="stylesheet" href="../styles/styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container-fluid" align="center">
+
+  <h2>Choix du type de véhicule pour les joueurs.</h2>
+
+    <div class="container">
+      <h4>Joueur 1</h4>
+      <select id="j1" onchange="choix_j1(this.value)" class="form-select" aria-label="Default select example">
+        <option value="voiture">voiture</option>
+        <option value="moto">moto</option>
+        <option value="bus">bus </option>
+      </select>
+    </div>
+
+    <div class="container">
+      <h4>Joueur 2</h4>
+      <select id="j2" onchange="choix_j2(this.value)" class="form-select" aria-label="Default select example">
+        <option value="voiture">voiture</option>
+        <option value="moto">moto</option>
+        <option value="bus">bus </option>
+      </select>
+    </div>
+
+  </div>
+
+  <br />
+
+
+  <div class="container" align="center">
     <form action="pagePrincipale.php">
-      <button type="submit" >page_principal </button>
+      <button type="submit" class="btn btn-outline-dark">page_principal </button>
     </form>
-    </body>
+  </div>
+
+</body>
+
 </html>
--- View/parametre.php
+++ View/parametre.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-    <script src="../Scripts/vehicule.js" defer></script>
-    <script src="../Scripts/script.js" defer></script>
-    <script src="../Scripts/parametre.js" defer></script>
-    </head>
-    <body>
-    <select id="sel" onchange="mode_de_jeu(this.value)">
-    <option value="solo">solo</option>
-    <option value="multi">multijoueur</option>
-    <option value="ordi">ordinateur </option>
-  </select>
-  <form action="pagePrincipale.php">
-    <button type="submit">page_principal </button>
-  </form>
+
+<head>
+  <script src="../Scripts/vehicule.js" defer></script>
+  <script src="../Scripts/script.js" defer></script>
+  <script src="../Scripts/parametre.js" defer></script>
+  <link rel="stylesheet" href="../styles/styles.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container-fluid" align="center">
+  <h4>Parametre du mode de jeu</h4>
+</div>
+<br />
+  <div class="container-fluid" align="center">
+    <select id="sel" onchange="mode_de_jeu(this.value)"class="form-select" aria-label="Default select example">
+      <option value="solo">solo</option>
+      <option value="multi">multijoueur</option>
+      <option value="ordi">ordinateur </option>
+    </select>
+    <div class="container">
+  <br/>
+      <form action="pagePrincipale.php">
+        <button type="submit" class="btn btn-outline-dark">page_principal </button>
+      </form>
+
+    </div>
+  </div>
+
 </body>
 
 </html>
